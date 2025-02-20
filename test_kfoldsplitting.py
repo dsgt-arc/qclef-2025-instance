bd9b0cde43610ca6ed502f6eed4788ed4609d891
palexbg <56965360+palexbg@users.noreply.github.com>
2025-02-20 09:14:45 +0100

    add kfold splitting
    Refer to Figure 3 of the Bcos paper. Test and validation are kept for the transformer, the IS is done only on the training data

---
+++ test_kfoldsplitting.py
@@ -0,0 +1,10 @@
+from src import utils
+import pandas as pd
+
+data = pd.read_csv("data/vader_nyt/bert_nytEditorialSnippets.csv")
+X = data.iloc[:, 1:-2].values
+Y = data.iloc[:, -2].values
+
+results = utils.k_fold(X, Y)
+
+print('a')
